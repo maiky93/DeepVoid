d2a35bdaa98fc8704e7eda3238f5da4e9f11a088
Maiky93 <maikyylive@gmail.com>
2022-08-13 23:59:09 +0200

    Initial commit

---
+++ src/main/java/net/maikyy/deepvoid/DeepVoid.java
@@ -0,0 +1,14 @@
+package net.maikyy.deepvoid;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeepVoid implements ModInitializer {
+	public static final String MOD_ID = "deepvoid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/maikyy/deepvoid/DeepVoidClient.java
@@ -0,0 +1,10 @@
+package net.maikyy.deepvoid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeepVoidClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
